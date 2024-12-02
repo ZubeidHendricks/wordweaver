3839f96275399d9a37b7b919dfca12bd9fa71f50
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-02 18:01:17 +0200

    feat: update AdComponent to placeholder

--- client/src/components/AdComponent.js
+++ client/src/components/AdComponent.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 const AdComponent = ({ adSlot }) => {
   return (
-    <div>
-      {/* Ad implementation will go here */}
-      <div style={{ textAlign: 'center', padding: '20px', background: '#f5f5f5' }}>
+    <div className="ad-container" style={{ textAlign: 'center', padding: '20px', background: '#f5f5f5', margin: '20px 0' }}>
+      <div style={{ fontStyle: 'italic', color: '#666' }}>
         Advertisement Space (Slot: {adSlot})
       </div>
     </div>
