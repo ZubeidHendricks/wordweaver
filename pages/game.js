b7d52bdf18fd1814c69e33f9b84bed5b0d9d8b7a
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-19 14:42:01 +0200

    Create game page to render Wordle game component

--- pages/game.js
+++ pages/game.js
@@ -1,17 +1,6 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Game from '../src/components/Game';
+import React from 'react';
+import WordleGame from '../src/components/Game';
 
-export default function GamePage({ user, showNotification }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!user && typeof window !== 'undefined') {
-      router.push('/login');
-    }
-  }, [user]);
-
-  if (!user) return null;
-
-  return <Game user={user} showNotification={showNotification} />;
+export default function GamePage() {
+  return <WordleGame />;
 }
